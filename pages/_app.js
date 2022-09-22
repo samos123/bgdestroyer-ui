615a379ab8d8e9f27edd51fddd91c88360833752
Sam Stoelinga <sammiestoel@gmail.com>
2022-09-22 14:09:16 -0700

    Move stylesheet to pages/_document.js

--- pages/_app.js
+++ pages/_app.js
@@ -10,15 +10,6 @@ config.autoAddCss = false;
 function MyApp({ Component, pageProps }) {
     return (
         <>
-            <Head>
-                <link
-                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-                    rel="stylesheet"
-                    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-                    crossOrigin="anonymous"
-                />
-            </Head>
-
             <Script
                 id="bootstrap-cdn"
                 src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
