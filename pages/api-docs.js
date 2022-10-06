fbeff8f1cda968cd689885549d2a97b912bce1f4
Sam Stoelinga <sammiestoel@gmail.com>
2022-10-06 14:54:04 -0700

    Add get jwt token to api docs

--- pages/api-docs.js
+++ pages/api-docs.js
@@ -1,11 +1,29 @@
 import dynamic from "next/dynamic";
-import axios from 'axios'
+import axios from "axios";
+import { auth } from "../lib/initFirebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import TokenButton from "../components/TokenButton";
 
 const SwaggerUI = dynamic(import("swagger-ui-react"), { ssr: false });
 import "swagger-ui-react/swagger-ui.css";
 
 const OpenAPIDocs = ({ doc }) => {
-    return <SwaggerUI spec={doc} />;
+    const [user, loading, error] = useAuthState(auth);
+
+    if (user) {
+        return (
+            <div>
+                <p>
+                    Hello {user.email}. Generate a JWT token below so you can
+                    call the API
+                </p>
+                <TokenButton className="mt-2" />
+                <SwaggerUI spec={doc} docExpansion="full" />
+            </div>
+        );
+    } else {
+        return <SwaggerUI spec={doc} docExpansion="full" />;
+    }
 };
 export default OpenAPIDocs;
 
