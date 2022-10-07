e5ba7276d61f8a0e8f785c3c62c2cb3cd55d5fc4
Sam Stoelinga <sammiestoel@gmail.com>
2022-10-07 13:32:27 -0700

    Add credits for logged in users

--- pages/profile.js
+++ pages/profile.js
@@ -1,16 +1,38 @@
-import { auth } from "../lib/initFirebase";
+import React, { useState, useEffect } from "react";
+import { auth, db } from "../lib/initFirebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import TokenButton from "../components/TokenButton";
+import { doc, getDoc } from "firebase/firestore";
 
 export default function Profile() {
     const [user, loading, error] = useAuthState(auth);
+    const [profile, setProfile] = useState(null);
+
+    useEffect(() => {
+        const fetchProfile = async () => {
+            console.log(user.uid)
+            const docRef = doc(db, "Users", user.uid);
+            const userSnap = await getDoc(docRef);
+            if (userSnap.exists()) {
+                setProfile(userSnap.data());
+            }
+        };
+        // call the function
+        fetchProfile().catch(console.error);
+    }, [user]);
 
     if (user) {
         return (
             <div className="center">
                 <p>
-                    Hello {user.email}. Generate a JWT token below so you can call the API</p>
+                    Hello {user.email}.                </p>
+                {profile && <p>Credits: {profile.credits}</p>}
+                <p>
+ Generate a JWT token below so you can
+                    call the API
+
                 <TokenButton className="mt-2" />
+            </p>
             </div>
         );
     }
